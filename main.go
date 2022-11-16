23af0631035d713ecb07a861894c4c1b6a4cd74c
ozan1338 <akhmadfauzan1338@gmail.com>
2022-11-16 10:09:44 +0700

    First Commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
+
+	// smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
+
+	// adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", order.Id, adminRevenue))
+
+	// smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
+}
