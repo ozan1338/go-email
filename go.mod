73c96f663eff8d0aeeda874b62ce9851f7cc2a29
ozan1338 <akhmadfauzan1338@gmail.com>
2022-11-17 10:48:42 +0700

    Connect With Kafka And Consume the Value

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-email
 
 go 1.18
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,58 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/smtp"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
 func main() {
-	// ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", ambassadorRevenue, order.Code))
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "pkc-ew3qg.asia-southeast2.gcp.confluent.cloud:9092",
+		"security.protocol": "SASL_SSL",
+		"sasl.username": "ZRITSDHTM4YORCX3",
+		"sasl.password": "iOJVSZ5sHVRnmunF7VvCw+lC1iADXyNZGeYuVlZZfUlvcvUn4fotwbsxRoW2WY2W",
+		"sasl.mechanism": "PLAIN",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	fmt.Println("START")
 
-	// smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{order.AmbassadorEmail}, ambassadorMessage)
+	if err != nil {
+		panic(err)
+	}
 
-	// adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed", order.Id, adminRevenue))
+	
 
-	// smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
+	consumer.SubscribeTopics([]string{"default"}, nil)
+	
+	defer consumer.Close()
+
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err != nil {
+			// The client will automatically try to recover from all errors.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+			return
+		}
+
+		fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		var message map[string]interface{}
+
+		json.Unmarshal(msg.Value, &message)
+
+		ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", message["ambassadorRevenue"], message["code"]))
+
+		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{message["ambassadorEmail"].(string)}, ambassadorMessage)
+
+		adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed",message["id"], message["adminRevenue"]))
+
+		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
+	}
+
+	
+
+
 }
