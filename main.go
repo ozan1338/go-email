e99645f069abf56167c2d224215958886818750e
ozan1338 <akhmadfauzan1338@gmail.com>
2022-11-30 14:10:11 +0700

    Add Dockerfile for prod

--- main.go
+++ main.go
@@ -5,16 +5,44 @@ import (
 	"fmt"
 	"net/smtp"
 
+	"os"
+
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+const (
+	BOOTSRAP_SERVER = "BOOTSTRAP_SERVERS"
+	SERCURITY_PROTOCOL = "SECURITY_PROTOCOL"
+	SASL_USERNAME = "SASL_USERNAME"
+	SASL_PASSWORD = "SASL_PASSWORD"
+	SASL_MECHANISM = "SASL_MECHANISM"
+	KAFKA_TOPIC = "KAFKA_TOPIC"
+	EMAIL_HOST = "EMAIL_HOST"
+	EMAIL_PORT = "EMAIL_PORT"
+	EMAIL_USERNAME = "EMAIL_USERNAME"
+	EMAIL_PASSWORD = "EMAIL_PASSWORD"
+)
+
+var (
+	bootstrap_server = os.Getenv(BOOTSRAP_SERVER)
+	security_protocol = os.Getenv(SERCURITY_PROTOCOL)
+	sasl_username = os.Getenv(SASL_USERNAME)
+	sasl_password = os.Getenv(SASL_PASSWORD)
+	sasl_mechanism = os.Getenv(SASL_MECHANISM)
+	kafka_topic = os.Getenv(KAFKA_TOPIC)
+	email_host = os.Getenv(EMAIL_HOST)
+	email_port = os.Getenv(EMAIL_PORT)
+	email_username = os.Getenv(EMAIL_USERNAME)
+	email_password = os.Getenv(EMAIL_PASSWORD)
 )
 
 func main() {
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "pkc-ew3qg.asia-southeast2.gcp.confluent.cloud:9092",
-		"security.protocol": "SASL_SSL",
-		"sasl.username": "ZRITSDHTM4YORCX3",
-		"sasl.password": "iOJVSZ5sHVRnmunF7VvCw+lC1iADXyNZGeYuVlZZfUlvcvUn4fotwbsxRoW2WY2W",
-		"sasl.mechanism": "PLAIN",
+		"bootstrap.servers": bootstrap_server,
+		"security.protocol": security_protocol,
+		"sasl.username": sasl_username,
+		"sasl.password": sasl_password,
+		"sasl.mechanism": sasl_mechanism,
 		"group.id":          "myGroup",
 		"auto.offset.reset": "earliest",
 	})
@@ -26,7 +54,7 @@ func main() {
 
 	
 
-	consumer.SubscribeTopics([]string{"email_topic"}, nil)
+	consumer.SubscribeTopics([]string{kafka_topic}, nil)
 	
 	defer consumer.Close()
 
@@ -50,18 +78,15 @@ func main() {
 
 		json.Unmarshal(msg.Value, &message)
 
-		// emailAmbassador, ok := message["ambassadorEmail"].(string)
-		// if !ok {
-		// 	emailAmbassador = "unknown"
-		// }
+		auth := smtp.PlainAuth("", email_username,email_password, email_host)
 
 		ambassadorMessage := []byte(fmt.Sprintf("You earned $%f from the link #%s", message.AmbassadorRevenue, message.Code))
 
-		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{message.AmbassadorEmail}, ambassadorMessage)
+		smtp.SendMail(fmt.Sprintf("%s:%s",email_host,email_port), auth, "no-reply@email.com", []string{message.AmbassadorEmail}, ambassadorMessage)
 
 		adminMessage := []byte(fmt.Sprintf("Order #%d with a total of $%f has been completed",message.Id, message.AdminRevenue))
 
-		smtp.SendMail("host.docker.internal:1025", nil, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
+		smtp.SendMail(fmt.Sprintf("%s:%s",email_host,email_port), auth, "no-reply@email.com", []string{"admin@admin.com"}, adminMessage)
 	}
 
 	
